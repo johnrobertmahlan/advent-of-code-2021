87aaf3a02a3343f96f842e445a85fd4ce753d530
John Robert Mahlan <johnrobertmahlan@gmail.com>
2021-12-01 21:18:24 -0700

    solve day 1 problem 2

---
+++ day_1/problem_2.rb
@@ -0,0 +1,8 @@
+arr = File.readlines("input.txt").map {|a| a.gsub("\n", "").to_i}
+
+increases = arr.each_cons(3).map {|triple| triple.reduce(:+)}.each_cons(2).reduce(0) do |inc, pair|
+  inc += 1 if pair.first < pair.last
+  inc
+end
+
+p increases
