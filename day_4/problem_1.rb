aeb48d4dc7bbbd52985a0fe66a0721b86e106bc2
John Robert Mahlan <johnrobertmahlan@gmail.com>
2021-12-07 22:06:10 -0700

    clean up comments but code fails with real input

--- day_4/problem_1.rb
+++ day_4/problem_1.rb
@@ -5,9 +5,6 @@ nums = bingo_numbers.split(",")
 
 bingo_boards = input[1..-1]
 
-# p "These are all the boards:"
-# p bingo_boards
-
 def get_first_bingo_board(boards)
   boards[1..5]
 end
@@ -44,14 +41,6 @@ def get_columns(board)
 end
 
 def call_a_number(nums, board)
-  # the idea here is to pass in the number called
-  # and then delete any instance of that number from every board
-  # so suppose num = 22
-  # we need to remove 22 from every board
-  # the boards themselves are arrays or strings, so I can gsub out the num on those
-  # and then call get_rows and get_columns to check if there are any that are EMPTY
-  # that's the win condition
-#   nums = nums.map {|n| n.to_i }
   
   board_one = get_first_bingo_board(board).map {|str| str.split(" ")}
   board_two = get_second_bingo_board(board).map {|str| str.split(" ")}
@@ -67,90 +56,21 @@ def call_a_number(nums, board)
             idx = board.index(arr)
             board[idx] = new_arr
             new_board = board
-            # p board
+            
             if board.any? {|b| b.empty? }
-            #   p board.find {|bd| bd.last == board.last}
-            #   puts "WE FOUND IT"
-            #   puts "IT'S THIS BOARD: #{board}"
               return [num, board]
-              # COOL SO THIS WILL RETURN THE UPDATED BOARD
-              # NOW I HAVE TO PAIR IT WITH THE ONE IT CAME FROM
             end
-            board
           end
         end
       end
-    # p board
-    # end
-    # p board
-    # p board.length
-    # p get_rows(board)
-    # p get_rows(board).any? {|b| b.empty? }
   end
-    
-#   until boards.any? {|board| board.any? {|b| b.empty?} }
-#     nums.each do |num|
-#         new_boards = boards.each do |board|
-#             board.map do |arr|
-#               arr = arr - [num.to_s] if arr.include?(num.to_s)
-#               arr
-#             end
-#         end
-#     end
-#   end
-      
-    
-  
-#   p "Here is the first board before anything happens:"
-#   p get_first_bingo_board(board).map {|str| str.split(" ")}
-#   # remove num from first bingo board:
-#   # OKAY THIS WORKS
-#   new_first_board = get_first_bingo_board(board).map {|str| str.split(" ")}.map do |arr|
-#     arr = arr - [num.to_s] if arr.include?(num.to_s)
-#     arr
-#   end
-# #   new_second_board = get_second_bingo_board(board).map {|str| str.gsub(/num/, "")}
-# #   new_third_board = get_third_bingo_board(board).map {|str| str.gsub(/num/, "")}
-#   p "And here is that same board after num is removed:"
-#   p new_first_board
 end
-
-# p "Here is the first bingo board:"
-# p get_first_bingo_board(bingo_boards)
-
-# p "Here is the second bingo board:"
-# p get_second_bingo_board(bingo_boards)
-
-# p "Here is the third bingo board:"
-# p get_third_bingo_board(bingo_boards)
-
-# p "Here are the rows of the first board:"
-# p get_rows(get_first_bingo_board(bingo_boards))
-
-# p "Here are the columns of the first board:"
-# p get_columns(get_first_bingo_board(bingo_boards))
-
-# nums = [22, 13, 17, 11, 0]
-# p bingo_boards
-
-# p get_first_bingo_board(bingo_boards).map {|str| str.split(" ")}
-# p get_second_bingo_board(bingo_boards).map {|str| str.split(" ")}
-# p get_third_bingo_board(bingo_boards).map {|str| str.split(" ")}
 
 board_one = get_first_bingo_board(bingo_boards).map {|str| str.split(" ")}
 board_two = get_second_bingo_board(bingo_boards).map {|str| str.split(" ")}
 board_three = get_third_bingo_board(bingo_boards).map {|str| str.split(" ")}
 
 boards = [board_one, board_two, board_three]
-
-p "This is the winner: "
-p call_a_number(nums, bingo_boards)
-
-# def check_boards(formatted_boards, bingo_boards, nums)
-#   winner = formatted_boards.find {|board| board.last == call_a_number(nums, bingo_boards).last}
-#   # NOW I HAVE TO RECOVER WHICH ROW OR COLUMN WON
-#   new_nums = winner.find {|w| w.all? {|n| nums.include?(n.to_i)}}
-# end
 
 def calculate_final_score(nums, bingo_boards)
   winner = call_a_number(nums, bingo_boards)
